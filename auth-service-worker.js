3d88522ccf60bed08a3f511a73a38e43cb55467a
Rodrigo <1rodrigo1a@gmail.com>
2024-10-24 16:30:43 -0700

    updated firestore.js with missing code from tutorial step 7

--- auth-service-worker.js
+++ auth-service-worker.js
@@ -1,58 +1,112 @@
+
+// auth-service-worker.js
+
 import { initializeApp } from "firebase/app";
-import { getAuth, getIdToken } from "firebase/auth";
-import { getInstallations, getToken } from "firebase/installations";
+import { getAuth, getIdToken, onAuthStateChanged } from "firebase/auth";
 
-// this is set during install
-let firebaseConfig;
+// Extract Firebase config from query string
+const serializedFirebaseConfig = new URLSearchParams(self.location.search).get(
+  "firebaseConfig"
+);
+if (!serializedFirebaseConfig) {
+  throw new Error(
+    "Firebase Config object not found in service worker query string."
+  );
+}
 
-self.addEventListener('install', event => {
-  // extract firebase config from query string
-  const serializedFirebaseConfig = new URL(location).searchParams.get('firebaseConfig');
-  
-  if (!serializedFirebaseConfig) {
-    throw new Error('Firebase Config object not found in service worker query string.');
-  }
-  
-  firebaseConfig = JSON.parse(serializedFirebaseConfig);
+const firebaseConfig = JSON.parse(serializedFirebaseConfig);
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+self.addEventListener("install", () => {
   console.log("Service worker installed with Firebase config", firebaseConfig);
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener("fetch", (event) => {
-  /* 
-  
-  FIX FROM https://github.com/firebase/friendlyeats-web/issues/295#issuecomment-2318574547
-  
-  FIX START 
-  */
-  if (!firebaseConfig) {
-    const serializedFirebaseConfig = new URL(location).searchParams.get(
-      "firebaseConfig"
-    );
-    firebaseConfig = JSON.parse(serializedFirebaseConfig);
-  }
-  /* FIX END */
-  const { origin } = new URL(event.request.url);
+  const { origin, pathname } = new URL(event.request.url);
   if (origin !== self.location.origin) return;
-  event.respondWith(fetchWithFirebaseHeaders(event.request));
+
+  // Use a magic URL to ensure that auth state is in sync between
+  // the client and the service worker
+  if (pathname.startsWith("/__/auth/wait/")) {
+    const uid = pathname.split("/").at(-1);
+    event.respondWith(waitForMatchingUid(uid));
+    return;
+  }
+
+  if (pathname.startsWith("/_next/")) return;
+
+  // Don't add headers to non-GET requests or those with an extension
+  // This helps with CSS, images, fonts, JSON, etc.
+  if ((event.request.method === "GET" || event.request.method === "POST") && !pathname.includes(".")) {
+    event.respondWith(fetchWithFirebaseHeaders(event.request));
+  }
 });
 
 async function fetchWithFirebaseHeaders(request) {
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth(app);
-  const installations = getInstallations(app);
-  const headers = new Headers(request.headers);
-  const [authIdToken, installationToken] = await Promise.all([
-    getAuthIdToken(auth),
-    getToken(installations),
-  ]);
-  headers.append("Firebase-Instance-ID-Token", installationToken);
-  if (authIdToken) headers.append("Authorization", `Bearer ${authIdToken}`);
-  const newRequest = new Request(request, { headers });
-  return await fetch(newRequest);
+  let authIdToken = await getAuthIdToken();
+  if (!authIdToken) {
+    // sleep for 0.25s
+    await new Promise((resolve) => setTimeout(resolve, 250));
+    authIdToken = await getAuthIdToken();
+  }
+  if (!authIdToken) {
+    // sleep for 0.25s
+    await new Promise((resolve) => setTimeout(resolve, 250));
+    authIdToken = await getAuthIdToken();
+  }
+  if (authIdToken) {
+    const headers = new Headers(request.headers);
+    headers.append("Authorization", `Bearer ${authIdToken}`);
+    request = new Request(request, { headers });
+  }
+  return await fetch(request).catch((reason) => {
+    console.error(reason);
+    return new Response("Fail.", {
+      status: 500,
+      headers: { "Content-Type": "text/html" },
+    });
+  });
 }
 
-async function getAuthIdToken(auth) {
-  await auth.authStateReady();
-  if (!auth.currentUser) return;
+async function waitForMatchingUid(_uid) {
+  const uid = _uid === "undefined" ? undefined : _uid;
+  await authStateReady();
+  await new Promise((resolve) => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user?.uid === uid) {
+        unsubscribe();
+        resolve();
+      }
+    });
+  });
+  return new Response(undefined, {
+    status: 200,
+    headers: { "Cache-Control": "no-store" },
+  });
+}
+
+function authStateReady() {
+  return new Promise((resolve) => {
+    if (auth.currentUser !== undefined) {
+      resolve();
+    } else {
+      const unsubscribe = onAuthStateChanged(auth, () => {
+        unsubscribe();
+        resolve();
+      });
+    }
+  });
+}
+
+async function getAuthIdToken() {
+  await authStateReady();
+  if (!auth.currentUser) return null;
   return await getIdToken(auth.currentUser);
 }
--- src/lib/firebase/firestore.js
+++ src/lib/firebase/firestore.js
@@ -41,59 +41,59 @@ const updateWithRating = async (
 	const newAverage = newSumRating / newNumRatings;
 
 	transaction.update(docRef, {
-			numRatings: newNumRatings,
-			sumRating: newSumRating,
-			avgRating: newAverage,
+		numRatings: newNumRatings,
+		sumRating: newSumRating,
+		avgRating: newAverage,
 	});
 
 	transaction.set(newRatingDocument, {
-			...review,
-			timestamp: Timestamp.fromDate(new Date()),
+		...review,
+		timestamp: Timestamp.fromDate(new Date()),
 	});
 };
 
 export async function addReviewToRestaurant(db, restaurantId, review) {
 	if (!restaurantId) {
-			throw new Error("No restaurant ID has been provided.");
+		throw new Error("No restaurant ID has been provided.");
 	}
 
 	if (!review) {
-			throw new Error("A valid review has not been provided.");
+		throw new Error("A valid review has not been provided.");
 	}
 
 	try {
-			const docRef = doc(collection(db, "restaurants"), restaurantId);
-			const newRatingDocument = doc(
-					collection(db, `restaurants/${restaurantId}/ratings`)
-			);
+		const docRef = doc(collection(db, "restaurants"), restaurantId);
+		const newRatingDocument = doc(
+			collection(db, `restaurants/${restaurantId}/ratings`)
+		);
 
-			// corrected line
-			await runTransaction(db, transaction =>
-					updateWithRating(transaction, docRef, newRatingDocument, review)
-			);
+		// corrected line
+		await runTransaction(db, transaction =>
+			updateWithRating(transaction, docRef, newRatingDocument, review)
+		);
 	} catch (error) {
-			console.error(
-					"There was an error adding the rating to the restaurant",
-					error
-			);
-			throw error;
+		console.error(
+			"There was an error adding the rating to the restaurant",
+			error
+		);
+		throw error;
 	}
 }
 
 function applyQueryFilters(q, { category, city, price, sort }) {
 	if (category) {
-			q = query(q, where("category", "==", category));
+		q = query(q, where("category", "==", category));
 	}
 	if (city) {
-			q = query(q, where("city", "==", city));
+		q = query(q, where("city", "==", city));
 	}
 	if (price) {
-			q = query(q, where("price", "==", price.length));
+		q = query(q, where("price", "==", price.length));
 	}
 	if (sort === "Rating" || !sort) {
-			q = query(q, orderBy("avgRating", "desc"));
+		q = query(q, orderBy("avgRating", "desc"));
 	} else if (sort === "Review") {
-			q = query(q, orderBy("numRatings", "desc"));
+		q = query(q, orderBy("numRatings", "desc"));
 	}
 	return q;
 }
@@ -104,35 +104,35 @@ export async function getRestaurants(db = db, filters = {}) {
 	q = applyQueryFilters(q, filters);
 	const results = await getDocs(q);
 	return results.docs.map(doc => {
-			return {
-					id: doc.id,
-					...doc.data(),
-					// Only plain objects can be passed to Client Components from Server Components
-					timestamp: doc.data().timestamp.toDate(),
-			};
+		return {
+			id: doc.id,
+			...doc.data(),
+			// Only plain objects can be passed to Client Components from Server Components
+			timestamp: doc.data().timestamp.toDate(),
+		};
 	});
 }
 
 export function getRestaurantsSnapshot(cb, filters = {}) {
 	if (typeof cb !== "function") {
-			console.log("Error: The callback parameter is not a function");
-			return;
+		console.log("Error: The callback parameter is not a function");
+		return;
 	}
 
 	let q = query(collection(db, "restaurants"));
 	q = applyQueryFilters(q, filters);
 
 	const unsubscribe = onSnapshot(q, querySnapshot => {
-			const results = querySnapshot.docs.map(doc => {
-					return {
-							id: doc.id,
-							...doc.data(),
-							// Only plain objects can be passed to Client Components from Server Components
-							timestamp: doc.data().timestamp.toDate(),
-					};
-			});
+		const results = querySnapshot.docs.map(doc => {
+			return {
+				id: doc.id,
+				...doc.data(),
+				// Only plain objects can be passed to Client Components from Server Components
+				timestamp: doc.data().timestamp.toDate(),
+			};
+		});
 
-			cb(results);
+		cb(results);
 	});
 
 	return unsubscribe;
@@ -152,7 +152,24 @@ export async function getRestaurantById(db, restaurantId) {
 }
 
 export function getRestaurantSnapshotById(restaurantId, cb) {
-	return;
+	if (!restaurantId) {
+		console.log("Error: Invalid ID received: ", restaurantId);
+		return;
+	}
+
+	if (typeof cb !== "function") {
+		console.log("Error: The callback parameter is not a function");
+		return;
+	}
+
+	const docRef = doc(db, "restaurants", restaurantId);
+	const unsubscribe = onSnapshot(docRef, docSnap => {
+		cb({
+			...docSnap.data(),
+			timestamp: docSnap.data().timestamp.toDate(),
+		});
+	});
+	return unsubscribe;
 }
 
 export async function getReviewsByRestaurantId(db, restaurantId) {
